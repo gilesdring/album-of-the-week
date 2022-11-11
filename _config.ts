7852b3f0ffc4e067922b213a1bb28dd07fcb4508
Giles Dring <giles@dringtech.com>
2022-11-11 08:22:44 +0000

    Minimal Lume config

---
+++ _config.ts
@@ -0,0 +1,14 @@
+import lume from "lume/mod.ts";
+import base_path from "lume/plugins/base_path.ts";
+
+const site = lume();
+
+site.use(base_path());
+
+site.ignore("src", "node_modules");
+
+site.loadPages([".html"]);
+site.loadAssets([".css"]);
+site.copy("/data");
+
+export default site;
