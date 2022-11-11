47f1935c286047dd29ae16e5276a2a6577053482
Giles Dring <giles@dringtech.com>
2022-11-11 08:39:46 +0000

    Add site location to lume build

--- _config.ts
+++ _config.ts
@@ -3,7 +3,9 @@ import base_path from "lume/plugins/base_path.ts";
 
 const site = lume();
 
-site.use(base_path());
+site.use(base_path({
+  location: new URL("https://gilesdring.com/album-of-the-week/")
+}));
 
 site.ignore("src", "node_modules");
 
